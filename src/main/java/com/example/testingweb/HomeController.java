459097bcf43a72296048e17eea9650e6b04ae494
Rodrigo Olivares <rodrigo.olivares@basedos.cl>
2022-09-05 01:59:55 -0300

    get

--- src/main/java/com/example/testingweb/HomeController.java
+++ src/main/java/com/example/testingweb/HomeController.java
@@ -2,12 +2,13 @@ package com.example.testingweb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-	@RequestMapping("/")
+	@RequestMapping(path = "/", method = RequestMethod.GET)
 	public @ResponseBody String greeting() {
 		return "Finaliza el curso DevOps Foundation v2 - Rodrigo Olivares";
 	}
